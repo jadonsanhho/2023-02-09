8b76c22fba8662d79bbf30597169b35acbf37295
jadonsanhho <124104235+jadonsanhho@users.noreply.github.com>
2023-02-09 09:50:27 +0900

    Add files via upload

---
+++ DataSourceTest.java
@@ -0,0 +1,22 @@
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/*.xml"})
+public class _______Test {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
